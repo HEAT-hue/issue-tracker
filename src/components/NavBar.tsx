0cdc01087231e09fe4965834d80c5efadfa357ee
might guy <onyejemeemmanuel65@gmail.com>
2023-12-20 06:38:55 +0100

    Completed nav bar styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import { NavLink } from "@/lib/definitions"
 import { FaBug } from "react-icons/fa";
 import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const links: NavLink[] = [
     { label: 'Dashboard', href: '/' },
@@ -27,13 +28,18 @@ const NavBar = () => {
             <ul className="flex gap-x-6">
                 {links.map((link: NavLink, index: number) => {
                     return (
-                        <li key={index} className={`${pathname === link.href ? 'text-zinc-800' : 'text-zinc-500 hover:text-zinc-800'} transition-colors`}>
+                        <li key={index}
+                            className={classNames({
+                                'text-zinc-800': pathname === link.href,
+                                'text-zinc-500': pathname !== link.href,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}>
                             <Link href={link.href}>{link.label}</Link>
                         </li>
                     )
                 })}
             </ul>
-        </nav>
+        </nav >
     )
 }
 
