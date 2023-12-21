4aafb2a2cff05197b25e42d7b239a923a7c857a0
might guy <onyejemeemmanuel65@gmail.com>
2023-12-21 03:31:17 +0100

    Build the new issue page

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import CreateIssueForm from '@/components/CreateIssueForm';
+// import CreateIssueForm from '@/components/CreateIssueForm';
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 const IssuesPage = () => {
 
   return (
-    <div className='p-6 flex flex-col gap-y-4'>
+    <div className='flex flex-col gap-y-4'>
       <div>IssuesPage</div>
-      <CreateIssueForm />
+      <Button><Link href={"/issues/new"}>Create Issue</Link></Button>
     </div>
   )
 }
