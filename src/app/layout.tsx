3a4b54f01ec657ec91f1cb44717d228ed8fd39dd
might guy <onyejemeemmanuel65@gmail.com>
2023-12-21 03:31:17 +0100

    Build the new issue page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children, }: {
         {/* Radix UI Theme */}
         <Theme>
           <NavBar />
-          <main>{children}</main>
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
