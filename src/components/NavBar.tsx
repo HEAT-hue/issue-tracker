39c48676733f4dab06b58ee32b2101ee2350c074
Emmanuel <onyejemeemmanuel65@gmail.com>
2023-12-28 07:26:32 +0100

    Changed the layout of the nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,6 +5,7 @@ import { FaBug } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
 import { signOutUser } from "@/lib/actions";
+import { Container, Flex } from "@radix-ui/themes";
 
 const links: NavLink[] = [
     { label: 'Dashboard', href: '/' },
@@ -21,37 +22,42 @@ const NavBar = () => {
     const pathname = usePathname();
 
     return (
-        <nav className="flex gap-x-6 px-5 h-14 items-center border-b">
-            {/* Logo */}
-            <Link href={'/'}><FaBug /></Link>
+        <nav className="flex gap-x-6 px-5 py-2 items-center border-b">
+            <Container>
+                <Flex justify={'between'}>
+                    <Flex align={'center'} gap={'3'}>
+                        {/* Logo */}
+                        <Link href={'/'}><FaBug /></Link>
 
-            {/* Nav Links */}
-            <ul className="flex gap-x-6">
-                {links.map((link: NavLink, index: number) => {
-                    return (
-                        <li key={index}
-                            className={classNames({
-                                'text-zinc-800': pathname === link.href,
-                                'text-zinc-500': pathname !== link.href,
-                                'hover:text-zinc-800 transition-colors': true
-                            })}>
-                            <Link href={link.href}>{link.label}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
+                        {/* Nav Links */}
+                        <ul className="flex gap-x-6">
+                            {links.map((link: NavLink, index: number) => {
+                                return (
+                                    <li key={index}
+                                        className={classNames({
+                                            'text-zinc-800': pathname === link.href,
+                                            'text-zinc-500': pathname !== link.href,
+                                            'hover:text-zinc-800 transition-colors': true
+                                        })}>
+                                        <Link href={link.href}>{link.label}</Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </Flex>
 
-            {pathname.startsWith("/dashboard") && (
-                <form
-                    className="ml-auto"
-                    action={signOutUser}
-                >
-                    <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                        <div className="hidden md:block">Sign Out</div>
-                    </button>
-                </form>
+                    {pathname.startsWith("/dashboard") && (
+                        <form
+                            action={signOutUser}
+                        >
+                            <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                                <div className="hidden md:block">Sign Out</div>
+                            </button>
+                        </form>
+                    )}
+                </Flex>
+            </Container>
 
-            )}
         </nav >
     )
 }
